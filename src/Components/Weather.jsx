a75d9bb726cd9f56f5ca002e4a077600e9f5d685
Diana Hutan <dianahutange@gmail.com>
2023-06-28 23:47:28 +0200

    boiler plate code; prepared the environment

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+function Weather() {
+const [data, setData] = useState({})
+const [location, setLocation] = useState ('')
+
+ const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=f09e3c80f7aef3f9f78738396e5ecf79`
+
+const searchLocation = (event) => {
+if (event.key === "Enter") {
+  axios.get(url).then((response) => {
+    setData(response.data)
+    console.log(response.data)
+     })
+     setLocation('')
+    }
+ }
+
+  return (
+  <div className="app">
+    <div className="search">
+      <input value = {location} 
+      onChange={event => setLocation (event.target.value)} 
+      onKeyUp= {searchLocation}
+      placeholder="Enter location" type = "text"/>
+    </div>
+    <div className="container">
+      <div className="top">
+      <div className="location">
+        <p>{data.name}</p>
+      </div>
+      <div className="temp">
+      {data.main ? <h1> {data.main.temp.toFixed()}°C</h1> : null}
+      </div>
+      <div className="description">
+        {data.weather ? <p>{data.weather[0].main}</p> : null}
+      </div>
+      </div>
+{data.name != undefined && 
+
+      <div className="bottom">
+      <div className="feels">
+        {data.main ? <p className="bold">{data.main.feels_like} </p> : null}
+        <p>Feels like</p>
+      </div>
+      <div className="humidity"> 
+      {data.main ? <p className="bold">{data.main.humidity.toFixed()}% </p> : null}
+      <p>Humidity</p>
+      </div>
+       <div className="wind">
+       {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MHP </p> : null}
+       <p>Wind speed</p>
+           </div>
+         </div>
+       }
+        </div>
+    </div>
+  )
+  }
+
+export default Weather
